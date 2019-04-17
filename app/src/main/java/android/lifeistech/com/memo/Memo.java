102d4dbf109eebc270956a9517de9c78ee73e906
Hirotoshi Kato <hirotoshi.kato@ucl.meijo-u.ac.jp>
2019-04-17 11:01:22 +0900

    原型となるメモアプリの状態

--- app/src/main/java/android/lifeistech/com/memo/Memo.java
+++ app/src/main/java/android/lifeistech/com/memo/Memo.java
@@ -1,4 +1,11 @@
 package android.lifeistech.com.memo;
 
-public class Memo {
+import io.realm.Realm;
+import io.realm.RealmObject;
+
+public class Memo extends RealmObject {
+    public String title;
+    public String updateDate;
+    public String content;
+
 }
